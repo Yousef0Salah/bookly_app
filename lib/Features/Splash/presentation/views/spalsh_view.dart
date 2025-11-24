f8106bfec7b8ecd0b91f4f4c3124c3c4b3d20230
Yousef0Salah <zoom28644@gmail.com>
2025-11-24 17:32:30 +0200

    First commit

---
+++ lib/Features/Splash/presentation/views/spalsh_view.dart
@@ -0,0 +1,15 @@
+
+import 'package:bookly/Features/Splash/presentation/views/widgets/splash_view_body.dart';
+import 'package:flutter/material.dart';
+
+class SpalshView extends StatelessWidget {
+  const SpalshView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const SplashViewBody(),
+      
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:bookly/Features/Splash/presentation/views/spalsh_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      theme: ThemeData.dark(),
+      home: SpalshView(),
+    );
+  }
+}
