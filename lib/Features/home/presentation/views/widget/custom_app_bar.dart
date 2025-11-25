70093e71f3d069bf3d6de42dd6cfbf30a7816b10
Yousef0Salah <zoom28644@gmail.com>
2025-11-25 17:46:40 +0200

    add custom app bar to home

---
+++ lib/Features/home/presentation/views/widget/custom_app_bar.dart
@@ -0,0 +1,25 @@
+
+import 'package:bookly/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 42),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(AssetsData.logo, height: 28),
+          IconButton(
+            icon: Icon(FontAwesomeIcons.magnifyingGlass, size: 24),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/Features/home/presentation/views/widget/home_view_body.dart
@@ -0,0 +1,12 @@
+import 'package:bookly/Features/home/presentation/views/widget/custom_app_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [CustomAppBar()]);
+  }
+}
+
