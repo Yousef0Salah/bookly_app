2b5e56be5266377f8737b65f0028df2e60269a73
Yousef0Salah <zoom28644@gmail.com>
2025-12-07 19:58:55 +0200

    add rating to book details

--- lib/Features/home/presentation/views/widget/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widget/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/Features/home/presentation/views/widget/book_rating.dart';
 import 'package:bookly/Features/home/presentation/views/widget/custom_book_details_app_bar.dart';
 import 'package:bookly/Features/home/presentation/views/widget/custom_book_item.dart';
 import 'package:bookly/core/utils/styles.dart';
@@ -35,6 +36,8 @@ class BookDetailsViewBody extends StatelessWidget {
               ),
             ),
           ),
+          const SizedBox(height: 18),
+          const BookRating(mainAxisAlignment: MainAxisAlignment.center),
         ],
       ),
     );
