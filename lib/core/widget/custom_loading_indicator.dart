b4c1373b3a04053e871287abe5d8ac5164b547b0
Yousef0Salah <zoom28644@gmail.com>
2026-01-24 20:26:45 +0200

    fix issue with loading

--- lib/core/widget/custom_loading_indicator.dart
+++ lib/core/widget/custom_loading_indicator.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class CustomLoadingIndicator extends StatelessWidget {
   const CustomLoadingIndicator({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: CircularProgressIndicator(),
+    return Center(child: CircularProgressIndicator());
+  }
+}
+
+class CustomShimmer extends StatelessWidget {
+  const CustomShimmer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer(
+      gradient: LinearGradient(
+        colors: [Colors.grey.shade500, Colors.white, Colors.grey.shade300],
+        stops: [0.1, 0.5, 0.9],
+      ),
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height * .3,
+        child: ListView.builder(
+          itemCount: 10,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: AspectRatio(
+                  aspectRatio: 2.6 / 4,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
